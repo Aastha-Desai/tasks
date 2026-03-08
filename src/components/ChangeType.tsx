94df8a23c4f00c701f93b62715f8360c0d1e59bd
Aastha Desai <aasthade@udel.edu>
2026-03-08 11:19:25 -0400

    Did task 8

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,19 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+    function switchType(): void {
+        type === "short_answer_question" ?
+            setType("multiple_choice_question")
+        :   setType("short_answer_question");
+    }
+
+    return (
+        <div>
+            <Button onClick={switchType}>Change Type</Button>
+            {type === "short_answer_question" ?
+                <p>Short Answer</p>
+            :   <p>Multiple Choice</p>}
+        </div>
+    );
 }
--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,14 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisible] = useState<boolean>(true);
+    function flipVisability(): void {
+        setVisible(!visible);
+    }
+    return (
+        <div>
+            <Button onClick={flipVisability}>Reveal Answer</Button>
+            {!visible && <div>42</div>}
+        </div>
+    );
 }
